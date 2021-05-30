022785e994d2addf17b45b3fbdb7e985f8c10f0d
AlokSinghKumar <ALOK743165@GMAIL.COM>
2021-05-30 08:58:04 +0530

    merge sort complete

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,8 +1,20 @@
-import React, { Component } from 'react'
-import './SortingVisualizer.css'
-import { mergeSort } from '../sortingAlgorithm/sortingAlgorithm';
+import React from 'react';
+import { getMergeSortAnimations } from '../sortingAlgorithm/sortingAlgorithm';
+import './SortingVisualizer.css';
 
-export default class SortingVisualizer extends Component {
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 1;
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 310;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = 'turquoise';
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = 'red';
+
+export default class SortingVisualizer extends React.Component {
     constructor (props) {
         super(props);
 
@@ -11,27 +23,68 @@ export default class SortingVisualizer extends Component {
         };
     }
 
-
     componentDidMount() {
         this.resetArray();
     }
 
     resetArray() {
         const array = [];
-        for (let i = 0; i < 310; i++) {
-            array.push(randomIntFromInterval(5, 560));
+        for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+            array.push(randomIntFromInterval(5, 730));
         }
-
         this.setState({ array });
-
     }
 
-
     mergeSort() {
-        const javaScriptSortedArray = this.state.array.slice().sort();
-        const sortedArray = mergeSort(this.state.array)
+        const animations = getMergeSortAnimations(this.state.array);
+        for (let i = 0; i < animations.length; i++) {
+            const arrayBars = document.getElementsByClassName('array-bar');
+            const isColorChange = i % 3 !== 2;
+            if (isColorChange) {
+                const [barOneIdx, barTwoIdx] = animations[i];
+                const barOneStyle = arrayBars[barOneIdx].style;
+                const barTwoStyle = arrayBars[barTwoIdx].style;
+                const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color;
+                }, i * ANIMATION_SPEED_MS);
+            } else {
+                setTimeout(() => {
+                    const [barOneIdx, newHeight] = animations[i];
+                    const barOneStyle = arrayBars[barOneIdx].style;
+                    barOneStyle.height = `${ newHeight }px`;
+                }, i * ANIMATION_SPEED_MS);
+            }
+        }
+    }
 
-        console.log(arrayAreEqual(javaScriptSortedArray, sortedArray));
+    quickSort() {
+        // We leave it as an exercise to the viewer of this code to implement this method.
+    }
+
+    heapSort() {
+        // We leave it as an exercise to the viewer of this code to implement this method.
+    }
+
+    bubbleSort() {
+        // We leave it as an exercise to the viewer of this code to implement this method.
+    }
+
+    // NOTE: This method will only work if your sorting algorithms actually return
+    // the sorted arrays; if they return the animations (as they currently do), then
+    // this method will be broken.
+    testSortingAlgorithms() {
+        for (let i = 0; i < 100; i++) {
+            const array = [];
+            const length = randomIntFromInterval(1, 1000);
+            for (let i = 0; i < length; i++) {
+                array.push(randomIntFromInterval(-1000, 1000));
+            }
+            const javaScriptSortedArray = array.slice().sort((a, b) => a - b);
+            const mergeSortedArray = getMergeSortAnimations(array.slice());
+            console.log(arraysAreEqual(javaScriptSortedArray, mergeSortedArray));
+        }
     }
 
     render() {
@@ -39,34 +92,40 @@ export default class SortingVisualizer extends Component {
 
         return (
             <div className="array-container">
-                { array.map((value, idx) => {
-                    return (
-                        < div
-                            className="array-bar"
-                            key={ idx }
-                            style={ { height: `${ value }px` } }
-                        ></div>
-                    )
-                }) }
-                <div>
-                    <button onClick={ () => this.resetArray() }>Generate New Array</button>
-                    <button onClick={ () => this.mergeSort() }>Merge sort</button>
-                </div>
+                { array.map((value, idx) => (
+                    <div
+                        className="array-bar"
+                        key={ idx }
+                        style={ {
+                            backgroundColor: PRIMARY_COLOR,
+                            height: `${ value }px`,
+                        } }></div>
+                )) }
+                <button onClick={ () => this.resetArray() }>Generate New Array</button>
+                <button onClick={ () => this.mergeSort() }>Merge Sort</button>
+                <button onClick={ () => this.quickSort() }>Quick Sort</button>
+                <button onClick={ () => this.heapSort() }>Heap Sort</button>
+                <button onClick={ () => this.bubbleSort() }>Bubble Sort</button>
+                <button onClick={ () => this.testSortingAlgorithms() }>
+                    Test Sorting Algorithms (BROKEN)
+                </button>
             </div>
-
-        )
+        );
     }
 }
 
+// From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
 function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min - 1) + min)
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function arrayAreEqual(arr1, arr2) {
-    if (arr1.length !== arr2.length) return false;
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) return false
+function arraysAreEqual(arrayOne, arrayTwo) {
+    if (arrayOne.length !== arrayTwo.length) return false;
+    for (let i = 0; i < arrayOne.length; i++) {
+        if (arrayOne[i] !== arrayTwo[i]) {
+            return false;
+        }
     }
-
-    return true
+    return true;
 }
